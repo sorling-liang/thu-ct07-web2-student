2b9a3e2461276ae2de23ef1a7f2cac85dd039fd9
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-13 04:13:12 +0000

    Jul 13, 2025, 4:13 AM

---
+++ p5play_intro/L01_redcircle.js
@@ -0,0 +1,25 @@
+// this is a comment
+let posx = 50;
+let posy = 50;
+
+// on green flag clicked
+function setup() {
+    // give me a blank drawing paper, size 300 width, 300 height
+    new Canvas(600, 300);
+    background("white"); // graytone background color
+}
+
+// forever block; refresh at 60 times per second
+function draw() {
+    background("white"); // graytone background color
+
+    fill("red"); 
+    // x    y    diameter
+circle(posx, posy, 50);
+posx = posx + 1;
+posy = posy + 0.41;
+fill("yellow"); 
+circle(150, 105, 50);
+fill("green"); 
+circle(150, 160, 50);
+}

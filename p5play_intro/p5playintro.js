f69ec0a78ca5be5bcefb64ef7953080699a188ab
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-17 10:50:32 +0000

    Jul 17, 2025, 10:50 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -28,7 +28,7 @@ function setup() {
   ball.color = "hotpink";
   ball.y = 0; // start from the top
   ball.vel.y = 3; // velocity in the Y direction
-  ball.bounciness = 1;
+  ball.bounciness = 0.9;
 
   world.gravity.y = 10;
 

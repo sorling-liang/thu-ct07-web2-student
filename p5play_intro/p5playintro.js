063adbb5b888b0eb49b78cec91996a2ee3f1d07f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-17 10:17:05 +0000

    Jul 17, 2025, 10:17 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,7 +1,7 @@
 function setup() {
   // Set up the canvas
   new Canvas(600, 400);
-  background(200); // gray-tone 
+  // background(200); // gray-tone 
   background("skyblue"); // can use color names
 
   // Basic shape testing

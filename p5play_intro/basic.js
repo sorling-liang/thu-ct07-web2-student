3bb519a4aab258d6a113a6c47892a54bc7c944f1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-24 10:16:32 +0000

    Jul 24, 2025, 10:16 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -19,7 +19,8 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
   // i++;  is the same as i=i+1;
-  //
+  // i--;  is the same as i=i-1;
+  
   for (let i=1; i<11; i=i-1) {
     console.log("counting " + i);
   }

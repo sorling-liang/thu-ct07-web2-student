b8dd01c97d4fd80d838f58bf259fe56f57e49a98
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-07 09:31:43 +0000

    Aug 7, 2025, 9:31 AM

---
+++ flappybird/flappybird_04.js
@@ -0,0 +1,17 @@
+// the final game
+
+// similar to "when green flag clicked"
+// run once
+function setup() {
+    // width, height
+    new Canvas(400, 600);
+    
+    // color name is fine
+    background("white");
+}
+
+// called repeatedly 60 times per 1 second
+// where interaction and animation
+function draw() {
+    // nothing for now
+}

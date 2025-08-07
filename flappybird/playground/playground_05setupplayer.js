ae7cf565d08b4d3ba0e05eec4153d5d3c58c79b9
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-07 10:14:27 +0000

    Aug 7, 2025, 10:14 AM

---
+++ flappybird/playground/playground_05setupplayer.js
@@ -0,0 +1,64 @@
+// Lesson 5
+// variables goes here
+let bird, floor; // sprites
+let bg, base; // images
+
+let flapMidImg, flapDownImg, flapUpImg; // bird images
+
+// load media files like images or sound effects
+function preload() {
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png'); // floor
+
+    // load 3 images into 3 variables
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    flapDownImg = loadImage('assets/yellowbird-downflap.png');
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+}
+
+
+// similar to "when green flag clicked"
+// run once
+function setup() {
+    //       width, height
+    new Canvas(400, 600); // smart phone
+    
+    // color name is fine
+    background("skyblue"); // gray
+
+    // bird sprite
+    bird = new Sprite();
+    bird.img = flapMidImg;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.mass = 2; // heavier = stronger pull from gravity
+    bird.drag = 0.02; // air resistance
+    bird.bounciness = 1.5;
+    bird.collider = "dynamic"; // can react to other sprite
+
+    world.gravity.y = 10;
+
+    floor = new Sprite();
+    floor.collider = "static"; // dont move, stay at the position
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.img = base;
+}
+
+// called repeatedly 60 times per 1 second
+// where interaction and animation
+function draw() {
+    // show the background image
+    // width: width of the canvas object
+    // height: height of the canvas object
+    image(bg, 0, 0, width, height);
+
+    // if (mouse.presses("left")) {
+    //     new Sprite(mouseX, mouseY, 30, 30, "dynamic");
+    // }
+    // if (mouse.presses("right")) {
+    //     new Sprite(mouseX, mouseY, 30, 30, "static");
+    // }
+}

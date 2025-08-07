a82909866eeebb4e044c1895d4ac1b0ddfdfb47e
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-07 09:47:19 +0000

    Aug 7, 2025, 9:47 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -21,7 +21,7 @@ function setup() {
     background("skyblue"); // gray
 
     floor = new Sprite();
-    floor.collider = "static";
+    floor.collider = "static"; // 
     floor.y = height - 50;
     floor.img = base;
 }

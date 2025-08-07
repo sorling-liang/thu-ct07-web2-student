fa12018dd16e421e347f64b6611840710b33f092
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-07 10:11:28 +0000

    Aug 7, 2025, 10:11 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -35,7 +35,7 @@ function setup() {
     bird.height = 30;
     bird.mass = 2; // heavier = stronger pull from gravity
     bird.drag = 0.02; // air resistance
-    bird.bounciness = 0.5;
+    bird.bounciness = 1.5;
     bird.collider = "dynamic"; // can react to other sprite
 
     world.gravity.y = 10;

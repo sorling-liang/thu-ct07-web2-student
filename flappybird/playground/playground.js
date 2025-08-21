d1138cf4e1eed8b068d5b6f9d454413cd5c3b524
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-21 09:48:54 +0000

    Aug 21, 2025, 9:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -108,6 +108,8 @@ function draw() {
     text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
     text('is moving: ' + bird.isMoving,     10, 40); // y must increase
     text('sleeping: '  + bird.sleeping,     10, 60); // y must increase
+    text('pipes count: '  + bird.sleeping,     10, 60); // y must increase
+
 }
 
 // my custom function

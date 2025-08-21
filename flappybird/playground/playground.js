b8c26e6057fa889313e6e64d5f42e7148cb5c30f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-21 09:38:50 +0000

    Aug 21, 2025, 9:38 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -115,6 +115,6 @@ function spawnPipePair() {
     let gap = 70;
     let midY = height/2;
 
-    bottomPipe = new Sprite(150, midY, 52, 320, "static");
+    bottomPipe = new Sprite(150, midY +200 +gap/2, 52, 320, "static");
     bottomPipe.img = pipeImg;
 }

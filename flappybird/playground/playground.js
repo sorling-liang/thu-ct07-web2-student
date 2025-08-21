db29465e8bb8523e791d2faa5525371904d8cd5e
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-21 09:47:51 +0000

    Aug 21, 2025, 9:47 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -115,12 +115,12 @@ function spawnPipePair() {
     let gap = 70;
     let midY = height/2;
 
-    bottomPipe = new Sprite(150, midY +200 +gap/2, 52, 320, "static");
+    bottomPipe = new Sprite(400, midY +200 +gap/2, 52, 320, "static");
     bottomPipe.img = pipeImg;
 
     pipeGroup.add(bottomPipe);
 
-    topPipe = new Sprite(150, midY -200 -gap/2, 52, 320, "static");
+    topPipe = new Sprite(400, midY -200 -gap/2, 52, 320, "static");
     topPipe.img = pipeImg;
     topPipe.rotation = 180;
 

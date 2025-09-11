d045ac4a43e4990516784b55741522b6dfd05601
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-11 10:01:46 +0000

    Sep 11, 2025, 10:01 AM

--- flappybird/playground/bluescreen.js
+++ flappybird/playground/bluescreen.js
@@ -63,6 +63,7 @@ function displayScore() {
     for ( let one of scoreDigitArray ) {
         // inside the repeating
         let onedigit = new scoreGroup.Sprite(middle+offset, height/2, 24, 36);
+        // shift all digits to the center of the screen
         onedigit.x = onedigit.x - ( (scoreDigitArray.length-1) *25/2 )
         onedigit.img = digitImgs[one];
         offset = offset + 25;

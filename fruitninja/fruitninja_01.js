dd4543b6c23e5c111e5687ec90d78496397d3f43
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-18 10:23:49 +0000

    Sep 18, 2025, 10:23 AM

--- fruitninja/fruitninja_01.js
+++ fruitninja/fruitninja_01.js
@@ -9,13 +9,12 @@ let gameStart = false;
 he checks the image size of dojo
 he guesstimate the size of the canvas himself
 */
-
 /*
 he can load bg image, by refering to flappybird game code
 */
-
 /*
 he is trying to draw Text (for game on and game over)
+see non-guided folder for 2 screenshots from demo game
 */
 function preload() {
     bg = loadImage("assets/dojobackground.png");

de815d68d35eb471113399c48fec84ab72f5e489
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-25 09:42:57 +0000

    Sep 25, 2025, 9:42 AM

--- fruitninja/fruitninja_02.js
+++ fruitninja/fruitninja_02.js
@@ -11,7 +11,7 @@ let soundfx;
 function preload() {
     bg = loadImage("assets/dojobackground.png");
     bgm = createAudio("assets/fruit-ninja-bgtrack.mp3");
-    soundfx = createAudio("assets/sfx_wing.mp3");
+    soundfx = createAudio("assets/fruit-ninja-combo.mp3");
 }
 
 function setup() {

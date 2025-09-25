07fc522d2d417884fcb792e65ee65c54857daaac
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-25 10:30:21 +0000

    Sep 25, 2025, 10:30 AM

--- fruitninja/fruitninja_02.js
+++ fruitninja/fruitninja_02.js
@@ -27,6 +27,7 @@ function draw() {
     image(bg, 0,0, width, height); // background image
 
     if (gameStart) {
+        if (fcStart < 0)
         fcStart = frameCount; // keep a record
         //displayGameOver();
         displayGameStats(); // game progress statistics

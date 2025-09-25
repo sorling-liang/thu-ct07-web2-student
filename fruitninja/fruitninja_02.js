f798866f0fe6ea352006d0c79d05314c6e97a715
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-25 10:07:13 +0000

    Sep 25, 2025, 10:07 AM

--- fruitninja/fruitninja_02.js
+++ fruitninja/fruitninja_02.js
@@ -69,7 +69,7 @@ function displayGameStats() {
     fill("white"); // pen color
     textSize(24);
     textAlign(CENTER, CENTER);
-    text("Missed: " + missed, width/2, 35); // y =35
+    text("Missed: " + missed, width/2, 30); // y =30
 }
 
 function displayGameOver() {

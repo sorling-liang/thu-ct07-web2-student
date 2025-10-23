5b23f1ed3e8bd19771564dd015fad274cb6e475c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-23 10:49:20 +0000

    Oct 23, 2025, 10:49 AM

--- fruitninja/text.js
+++ fruitninja/text.js
@@ -5,6 +5,8 @@ let starWars = [
     "and this is their story..",
 ];
 
+let xpos, ypos;
+let textY;
 
 function setup() {
     new Canvas(400, 600);
@@ -12,9 +14,11 @@ function setup() {
 }
 
 function draw() {
+    // how to erase?
     fill("white"); // text colour
     textAlign(CENTER, CENTER);
     textSize(18); // font size
     // write a loop to draw the text in the array here
+    for(let index=0; index<starWars.length; index++)
     text("Hello MEE", width/2, height/2);
 }

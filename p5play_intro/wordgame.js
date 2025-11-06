8950f88c75086f61676245778bed91f4a1367689
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-11-06 10:31:55 +0000

    Nov 6, 2025, 10:31 AM

--- p5play_intro/wordgame.js
+++ p5play_intro/wordgame.js
@@ -21,7 +21,7 @@ function setup() {
 
     button1 = createButton("Guess");
     button1.position(width/2+30, 250);
-    button1.mousePressed()
+    button1.mousePressed( handleUserInput );
     //button1.size(150, 150);
 }
 
